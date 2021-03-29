ac24afe7c4242575e77191549a728fdd66bdd870
kshama <kshamamd@gmail.com>
2021-03-30 00:53:26 +0530

    checking last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -7,19 +7,38 @@ import java.util.Scanner;
 
 
 public class UserRegistration{
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    public void firstName() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter First Name: ");
-        String firstName = input.next();
+        String firstName = sc.next();
         String regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(firstName);
         boolean result = matcher.matches();
         if(result == true)
-            System.out.println("First Name is Valid!!");
+            System.out.println("First Name is Valid...");
         else
-            System.out.println("First Name is not Valid!!");
-
+            System.out.println("First Name is not valid...");
     }
 
+    public void lastName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter last Name: ");
+        String lastName = sc.next();
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(lastName);
+        boolean result = matcher.matches();
+        if(result == true)
+            System.out.println("Last Name is Valid...");
+        else
+            System.out.println("Lasst Name is not valid...");
+    }
+
+
+    public static void main(String[] args) {
+        UserRegistration regex = new UserRegistration();
+        regex.firstName();
+        regex.lastName();
+    }
 }
